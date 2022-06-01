3713274098262eecae03e6c3c9e1205874693753
Gary Smith <garysmith193358@gmail.com>
2022-06-01 22:37:19 +0000

    layout almost set up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import { Visualizer } from './Visualizer'
+import * as p5 from 'react-p5'
+import Sketch from 'react-p5';
+import 'p5/lib/addons/p5.sound';
+
+
+
+let currentSound;
+  const _p5 = new window.p5()
+  console.log(_p5)
 
 function App() {
+  const [audio, setAudio] = useState('')
+  const setup = (p5, canvasParentRef) => {
+    p5.createCanvas(200, 200).parent(canvasParentRef)
+  }
+  
+  useEffect(() => {
+    preload()
+  },[audio])
+  
+  const draw = (p5) => {
+    p5.background(255, 130, 20)
+    p5.ellipse(100, 100, 100)
+    p5.ellipse(300, 100, 100)
+  }
+  
+  const preload = () => {
+    currentSound = _p5.loadSound(audio)
+  }
+  
+   const mouseClicked = () => {
+            if (currentSound.isPlaying()) {
+                if (currentSound){
+                    currentSound.pause()
+                }
+            } else {
+                currentSound.play()
+            }
+        }
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+     <div>
+      <input type="file" name="file" accept="audio/*" onChange={(event) => setAudio(event.target.files[0])}/>
+      <button onClick={() => currentSound.play()}> Submit </button>
+      {audio ? 'there is audio' : 'we no have that'}
     </div>
-  );
+    <Sketch setup={setup} draw={draw} preload={preload} mouseClicked={mouseClicked} />
+    </>
+    )
+  
 }
 
-export default App;
+export default App
