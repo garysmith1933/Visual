e1e1b233f60f28387caac47b1d89a91f87a1d4d7
Gary Smith <garysmith193358@gmail.com>
2022-06-03 02:20:59 +0000

    the setup is a work in progress

--- src/App.js
+++ src/App.js
@@ -1,55 +1,83 @@
 import React, { useState, useEffect } from 'react'
-import { Visualizer } from './Visualizer'
-import * as p5 from 'react-p5'
 import Sketch from 'react-p5';
 import 'p5/lib/addons/p5.sound';
 
-
-
 let currentSound;
-  const _p5 = new window.p5()
-  console.log(_p5)
+let fft;
+
+// react-p5 has this so we can use p5 methods outside of draw, and set up.
+  const myp5 = new window.p5()
+   //to get access to the function on line 18, I need to get the prototype of this instance of P5 constructor
+  const P5 = Object.getPrototypeOf(myp5).constructor
+ 
 
 function App() {
   const [audio, setAudio] = useState('')
-  const setup = (p5, canvasParentRef) => {
-    p5.createCanvas(200, 200).parent(canvasParentRef)
+  
+  //function that is passed to the sketch component as a prop
+  const setup = (p, canvasParentRef) => {
+    console.log(p)
+    p.createCanvas(200, 200).parent(canvasParentRef)
+    fft = new P5.FFT()
+  console.log(fft)
   }
   
+    //function that is passed to the sketch component as a prop
+  const draw = (p) => {
+    p.background(255, 130, 20)
+    p.ellipse(100, 100, 100)
+    p.ellipse(300, 100, 100)
+    
+    // const wave = fft.waveForm()
+    
+    //   for (let i = 0; i < p.width; i++) {
+    //   let index = p.map()
+    // }
+    
+  }
+  
+
+  //sets the currentSound to the audio
+  const preload = () => {
+    currentSound = myp5.loadSound(audio)
+    console.log(currentSound)
+  }
+  
+    //any time audio is changed, run preload
   useEffect(() => {
     preload()
   },[audio])
+
   
-  const draw = (p5) => {
-    p5.background(255, 130, 20)
-    p5.ellipse(100, 100, 100)
-    p5.ellipse(300, 100, 100)
-  }
-  
-  const preload = () => {
-    currentSound = _p5.loadSound(audio)
-  }
-  
-   const mouseClicked = () => {
-            if (currentSound.isPlaying()) {
+  //current problem is that if i press once, it gets invoked twice, thus leading to a pause then play.
+  const mouseClicked = () => {
+    console.log(currentSound.isPlaying())
+     if (currentSound.isPlaying()) {
                 if (currentSound){
                     currentSound.pause()
                 }
             } else {
                 currentSound.play()
             }
-        }
+  }    
+  
+    const playSong = () => {
+      // fft.analyze()
+      currentSound.play()
+      console.log("song starting")
+    }
 
   
   return (
-    <>
-     <div>
+    <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+      <div>
+        <Sketch setup={setup} draw={draw} preload={preload} mouseClicked={mouseClicked} />
+      </div>
+      
       <input type="file" name="file" accept="audio/*" onChange={(event) => setAudio(event.target.files[0])}/>
-      <button onClick={() => currentSound.play()}> Submit </button>
+      <button onClick={playSong}> Submit </button>
       {audio ? 'there is audio' : 'we no have that'}
     </div>
-    <Sketch setup={setup} draw={draw} preload={preload} mouseClicked={mouseClicked} />
-    </>
     )
   
 }
